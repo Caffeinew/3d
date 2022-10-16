0b5ade6661941c420a233252eb240f20e22af4b8
Nikita Tarabrin <nikita.tarabrin@inspirebrands.com>
2022-10-16 17:36:04 +0500

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,50 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import Model from "../components/Sasamba";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const Home = () => {
+  const canvasRef = React.useRef(null);
 
-export default IndexPage
+  // React.useEffect(() => {
+  //   const ctx = canvasRef.current?.getContext("2d");
+
+  //   const handleResize = () => {
+  //     ctx.canvas.height = window.innerHeight;
+  //     ctx.canvas.width = window.innerWidth;
+  //   };
+
+  //   handleResize();
+  //   window.addEventListener("resize", handleResize);
+
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
+
+  return (
+    <div className="w-full h-screen">
+      <Canvas ref={canvasRef}>
+        <CameraController />
+        <React.Suspense fallback={null}>
+        <pointLight position={[10, 10, 10]} />
+          <ambientLight intensity={0.5} />
+          <Model />
+        </React.Suspense>
+      </Canvas>
+    </div>
+  );
+};
+
+export default Home;
+
+const CameraController = () => {
+  const { camera, gl } = useThree();
+  React.useEffect(() => {
+    const controls = new OrbitControls(camera, gl.domElement);
+    controls.minDistance = 3;
+    controls.maxDistance = 20;
+    return () => {
+      controls.dispose();
+    };
+  }, [camera, gl]);
+  return null;
+};
